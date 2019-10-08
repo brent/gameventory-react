8e8597c2bb547db7524bc7db3e54c1a78f311eab
brent <brent.e.meyer@gmail.com>
2019-10-07 20:57:29 -0400

    adds game search route and base view

--- src/routes/Index.js
+++ src/routes/Index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import GamesListContainer from '../containers/GamesListContainer';
 import GameDetail from '../views/GameDetail';
+import GameSearch from '../views/GameSearch';
 
 function Index() {
   return (
@@ -10,6 +11,7 @@ function Index() {
       <Switch>
         <Route exact path='/' component={ GamesListContainer } />
         <Route path='/game' component={ GameDetail } />
+        <Route path='/search' component={ GameSearch } />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/views/GameSearch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GameSearch(props) {
+  return (
+    <div className='gameSearch'>
+      <h2>Game search</h2>
+    </div>
+  );
+}
+
+export default GameSearch;
