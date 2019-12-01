cca31a90ec6b3ec168ccf2f82f343e0beb4e0ac5
brent <brent.e.meyer@gmail.com>
2019-12-01 17:06:26 -0500

    fixes broken list add interaction in ListsList

--- src/components/ListsList.js
+++ src/components/ListsList.js
@@ -7,14 +7,29 @@ function ListsList(props) {
   function renderLists(lists) {
     if (lists.length > 0) {
       return lists.map((list) => {
-        return (
-          <li key={ list.id }>
-            <Link to={`/lists/${list.id}`}>
-              <div>{ list.name }</div>
-              <div>{ list.description }</div>
-            </Link>
-          </li>
-        );
+        let listItem;
+
+        if (props.onClick) {
+          listItem = (
+            <li key={ list.id } onClick={ (e) => props.onClick(list.id) }>
+              <a href="">
+                <div>{ list.name }</div>
+                <div>{ list.description }</div>
+              </a>
+            </li>
+          );
+        } else {
+          listItem = (
+            <li key={ list.id }>
+              <Link to={`/lists/${list.id}`}>
+                <div>{ list.name }</div>
+                <div>{ list.description }</div>
+              </Link>
+            </li>
+          );
+        }
+
+        return listItem;
       });
     } else {
       return <p>nothing here</p>
